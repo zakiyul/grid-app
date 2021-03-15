77d33a03877513071f49f76ec371cbc60b858e82
zakiyul <zakiyul.py@gmail.com>
2021-03-15 17:48:42 +0800

    buat component btn

--- src/components/AddTileButton/index.js
+++ src/components/AddTileButton/index.js
@@ -1,5 +1,20 @@
 import React from "react";
+import { func, string } from "prop-types";
 
-export default function AddTileButton() {
-  return <div></div>;
+export default function AddTileButton({ onClick, color }) {
+  const btnStyle = {
+    backgroundColor: color,
+    display: "inline-block",
+    width: 20,
+    height: 20,
+    cursor: "pointer",
+    border: "1px solid black",
+  };
+
+  return <div style={btnStyle} onClick={onClick}></div>;
 }
+
+AddTileButton.propTypes = {
+  onClick: func.isRequired,
+  color: string.isRequired,
+};
